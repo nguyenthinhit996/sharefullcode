79d2f58927313ae3c3864e6ba9adcfc1c7d0190c
peter <nguyenthinhit996@gmail.com>
2020-12-17 21:20:19 +0700

    update stream api

--- Learn Java/NewFeatureSE8/src/featuresameple/streamapi/SteamImatermediateOperations.java
+++ Learn Java/NewFeatureSE8/src/featuresameple/streamapi/SteamImatermediateOperations.java
@@ -1,5 +1,41 @@
 package featuresameple.streamapi;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class SteamImatermediateOperations {
-	
+
+	public static void main(String[] args) {
+
+		System.out.println("\n-----------use Map---------------");
+		// use Map, change value of element
+		Stream<Integer> streamMapFromList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 89).stream();
+		streamMapFromList.map(num -> num * 2).forEach(s -> System.out.print(s + " "));
+
+		System.out.println("\n-----------filter method---------------");
+
+		// filter method, filter value
+		Stream<Integer> streamFilterFromList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 89).stream();
+		streamFilterFromList.filter(s -> {
+			if (s % 2 == 0) {
+				return true;
+			}
+			return false;
+		}).forEach(s -> System.out.print(s + " "));
+
+		System.out.println("\n------------sorted ascending default--------------");
+		// sorted ascending default
+		Stream<Integer> streamSortedFromList = Arrays.asList(111, 1, 2, 3, 4, 5, 6, 7, 8, 89).stream();
+		streamSortedFromList.sorted().forEach(s -> System.out.print(s + " "));
+
+		System.out.println("\n------------sorted decrease--------------");
+		// sorted decrease
+		Stream<Integer> streamSortedDecreaseFromList = Arrays.asList(111, 1, 2, 3, 4, 5, 6, 7, 8, 89).stream();
+		streamSortedDecreaseFromList.sorted((o1, o2) -> {
+			if (o1 < o2) {
+				return 1;
+			}
+			return -1;
+		}).forEach(s -> System.out.print(s + " "));
+	}
 }
--- Learn Java/NewFeatureSE8/src/featuresameple/streamapi/Streamapisample.java
+++ Learn Java/NewFeatureSE8/src/featuresameple/streamapi/Streamapisample.java
@@ -1,36 +1,70 @@
 package featuresameple.streamapi;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
-import java.util.StringJoiner;
-import java.util.stream.Collector;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Streamapisample {
+
+	void recipe() {
+		// reuse stream
+		Supplier<Stream<String>> stream2 = () -> {
+			return Arrays.stream(new String[] { "1", "1", "5", "1", "1" });
+		};
+
+		System.out.println("--------------average-----------------");
+		// average
+		stream2.get().mapToInt(Integer::parseInt).average().ifPresent(System.out::println);
+
+		System.out.println("--------------max-----------------");
+		int max = stream2.get().mapToInt(Integer::parseInt).max().getAsInt();
+		System.out.println(max);
+
+		System.out.println("--------------mapToObj-----------------");
+		stream2.get().mapToInt(Integer::parseInt).mapToObj(s -> "str" + s).forEach(System.out::println);
+	}
+
+	static void imtermediateOnlyExcuteNotPresent() throws IOException {
+		// create stream
+		Path path=Paths.get("readobject");
+		Supplier<Stream<String>> streamDemo = () -> {
+			try {
+				return Files.lines(path);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		};
+		
+		//demo imtermediate, not appearance thing
+		System.out.println("------------ only imtermediate------------------");
+		streamDemo.get().filter(s -> {
+			 if(s.contains("a1")) {
+				 System.out.println("filter 1: "+s);
+				 return true;
+			 }
+			 return false;
+		});
+		
+		// demo imtermediate with terminal, it appearance imtermediate and terminal
+		System.out.println("-------------imtermediate with terminal-----------------");
+		streamDemo.get().filter(s -> {
+			 if(s.contains("a1")) {
+				 System.out.println("filter 2: "+ s);
+				 return true;
+			 }
+			 return false;
+		}).forEach(System.out::println);
 	
-	public static void main(String[] args) {
-		List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
-
-		for (String in : myList) {
-			System.out.println(" " + in);
-		}
-
-		myList.stream().filter(s -> s.startsWith("c")).map(String::toUpperCase).sorted().forEach(System.out::println);
-
-		for (String in : myList) {
-			System.out.println(" - " + in);
-		}
-		
-		
+	}
+	
+	static void imtermediateOnlyExcuteNotPresent2() {
 		Stream.of("d2", "a2", "b1", "b3", "c")
-	    .filter(s -> {
-	        System.out.println("filter: " + s);
-	        return true;
-	    }).forEach(s-> System.out.println("for: " + s));
-		
-		System.out.println("--------------------------------");
-		
-		Stream.of("aa2", "a2", "b1", "b3", "c")
 	    .map(s -> {
 	        System.out.println("map: " + s);
 	        return s.toUpperCase();
@@ -39,49 +73,45 @@ public class Streamapisample {
 	        System.out.println("anyMatch: " + s);
 	        return s.startsWith("A");
 	    });
-		
-		System.out.println("--------------------------------");
-		
-		List<AAA> persons =
-			    Arrays.asList(
-			        new AAA("Max", 18),
-			        new AAA("Peter", 23),
-			        new AAA("Pamela", 23),
-			        new AAA("David", 12));
-		
-		Collector<AAA, StringJoiner, String> personNameCollector =
-			    Collector.of(
-			        () -> new StringJoiner(" | "),          // supplier
-			        (j, p) -> {
-			        	 System.out.println("j: " + j);
-			        	 System.out.println("p: " + p);
-			        	 j.add(p.name.toUpperCase());
-			        },  // accumulator
-			        (j1, j2) ->   
-			        	 
-			        	j1.merge(j2) 
-					 ,               // combiner
-			        StringJoiner::toString);                // finisher
 
-			String names = persons
-			    .stream()
-			    .collect(personNameCollector);
-
-			System.out.println(names); 
 	}
-}
-
-class AAA {
-    String name;
-    int age;
-
-    AAA(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
+	
+	static void notOptimized() {
+		Stream.of("d2", "a2", "b1", "b3", "c")
+	    .sorted((s1, s2) -> {
+	        System.out.printf("sort: %s; %s\n", s1, s2);
+	        return s1.compareTo(s2);
+	    })
+	    .filter(s -> {
+	        System.out.println("filter: " + s);
+	        return s.startsWith("a");
+	    })
+	    .map(s -> {
+	        System.out.println("map: " + s);
+	        return s.toUpperCase();
+	    })
+	    .forEach(s -> System.out.println("forEach: " + s));
+	}
+	
+	static void optimized() {
+		Stream.of("d2", "a2", "b1", "b3", "c")
+	    
+	    .filter(s -> {
+	        System.out.println("filter: " + s);
+	        return s.startsWith("a");
+	    })
+	    .sorted((s1, s2) -> {
+	        System.out.printf("sort: %s; %s\n", s1, s2);
+	        return s1.compareTo(s2);
+	    })
+	    .map(s -> {
+	        System.out.println("map: " + s);
+	        return s.toUpperCase();
+	    })
+	    .forEach(s -> System.out.println("forEach: " + s));
+	}
+	
+	public static void main(String[] args) throws IOException {
+		optimized();
+	}
 }
