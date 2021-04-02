28c5a97b8b354445ba0f5ba682ab0e416234805d
peter <nguyenthinhit996@gmail.com>
2021-04-02 14:56:25 +0700

    commit  this file file Learn Java/NewFeatureSE9/Structure Project Module/InModule/src/module-info.java

--- Learn Java/NewFeatureSE9/Structure Project Module/InModule/src/module-info.java
+++ Learn Java/NewFeatureSE9/Structure Project Module/InModule/src/module-info.java
@@ -2,6 +2,15 @@ module helloWorldModule {
 	
 	exports com.sharefullcode.blogspot;
 	// use StringUtils
-	requires org.apache.commons.lang3;
+//	requires org.apache.commons.lang3;
 	
+}
+
+module helloWorldModule2 {
+	
+	exports com.sharefullcode.blogspot;
+	// use StringUtils
+//	requires org.apache.commons.lang3;
+	// adddd
+	//exports com.sharefullcode.blogspot;
 }
